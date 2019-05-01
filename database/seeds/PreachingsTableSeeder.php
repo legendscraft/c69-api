20c1b0cd8534fb9ad308a4ab416f25b5deb01d67
Richard Omoka <richard.omoka@gmail.com>
2019-05-02 01:02:22 +0300

    Started work on preaching

---
+++ database/seeds/PreachingsTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class PreachingsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+    }
+}
