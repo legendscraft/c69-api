c0ae95bb8af87b886e86c8fcceafa0b0672ccdfa
Richard Omoka <romoka@strathmore.edu>
2019-06-27 15:04:25 +0300

    Ready to send report on email

---
+++ app/Traits/C69SharedTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Appointment extends Model
+{
+    protected $fillable = [
+        'name', 'user_id','gender_id','centre_id','appointment_frequency_id','last_met','is_late'
+    ];
+
+    public function gender()
+    {
+        return $this->belongsTo('App\Gender','gender_id');
+    }
+
+    public function frequency()
+    {
+        return $this->belongsTo('App\AppointmentFrequency','appointment_frequency_id');
+    }
+
+    public function centre()
+    {
+        return $this->belongsTo('App\Centre','centre_id');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany('App\AppointmentComment');
+
+    }
+}
