687e581bacaa10fa12720d59e581d09764adc8ef
Richard Omoka <romoka@strathmore.edu>
2019-07-04 14:39:31 +0300

    -preachings first

--- app/Traits/C69SharedTrait.php
+++ app/Traits/C69SharedTrait.php
@@ -16,14 +16,6 @@ trait C69SharedTrait
 
         $payload = array();
         $user = auth()->user();
-        $sacraments =  SacramentRecord::join('sacraments','sacrament_records.sacrament_id','=','sacraments.id')
-            ->where('user_id',intval($user->id))
-            ->whereBetween('record_date',[$start_date,$end_date])
-            ->select('name',DB::raw('sum(dcount) as recs'))
-            ->groupBy('name')->get();
-        $sacrement_report = array("title"=>"Sacraments",'records'=>$sacraments);
-        array_push($payload,$sacrement_report);
-
         $preachings =  PreachingRecord::join('preachings','preaching_records.preaching_id','=','preachings.id')
             ->where('user_id',intval($user->id))
             ->whereBetween('record_date',[$start_date,$end_date])
@@ -31,6 +23,14 @@ trait C69SharedTrait
             ->groupBy('name')->get();
         $preachings_report = array("title"=>"Preaching",'records'=>$preachings);
         array_push($payload,$preachings_report);
+
+        $sacraments =  SacramentRecord::join('sacraments','sacrament_records.sacrament_id','=','sacraments.id')
+            ->where('user_id',intval($user->id))
+            ->whereBetween('record_date',[$start_date,$end_date])
+            ->select('name',DB::raw('sum(dcount) as recs'))
+            ->groupBy('name')->get();
+        $sacrement_report = array("title"=>"Sacraments",'records'=>$sacraments);
+        array_push($payload,$sacrement_report);
         return $payload;
 
     }
