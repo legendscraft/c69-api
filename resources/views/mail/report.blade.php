fb85a80b06b037f28bd5393184a800b140cd6121
root <root@apps.strathmore.edu>
2020-02-12 12:44:50 +0300

    Fixed the report.blade merging error

--- resources/views/mail/report.blade.php
+++ resources/views/mail/report.blade.php
@@ -1,11 +1,7 @@
 @extends('layouts.mail')
 @section('content')
     <p  style="color: #464c50;font-size: 16px;line-height: 25px;margin: 0 0 20px;text-align: left!important;">Hello there,&nbsp;</p>
-<<<<<<< HEAD
-    <p  style="color: #464c50;font-size: 16px;line-height: 25px;margin: 0 0 20px;text-align: left!important;">{{$xmessage}}.&nbsp;</p>
-=======
    <p  style="color: #464c50;font-size: 16px;line-height: 25px;margin: 0 0 20px;text-align: left!important;">{{$xmessage}}.&nbsp;</p>
->>>>>>> f0729ebd63b5f8ef6574a4a6f67e4bc440d51636
     <p  style="color: #464c50;font-size: 16px;line-height: 25px;margin: 0 0 20px;text-align: left!important;">
         <i>Best Regards,<br>{{env('APP_NAME','C69 System')}}</i>
     </p>
